67afa6bf0d598f014f5c9011a2274bce859939f1
Vedant Bhoyar <vedantby@gmail.com>
2023-11-08 07:53:24 +0530

    added animation

--- src/app/(web)/page.tsx
+++ src/app/(web)/page.tsx
@@ -1,16 +1,33 @@
+"use client"
 import React from "react"
+import {motion} from "framer-motion"
 
 type Props = {}
 
 const Page = (props: Props) => {
     return (
-        <div>
-            <div className="text-5xl uppercase font-bold tracking-wider">
+        <div className="flex-col flex items-center justify-center ">
+            <motion.span 
+            initial={{opacity: 0, y: -10}}
+            animate={{opacity: 1, y:0}}
+            transition={{
+                animation: "easeInOut",
+                duration: 0.25
+            }}
+            className="text-5xl uppercase font-bold tracking-wider text-center">
                 Kodecs Technology
-            </div>
-            <div className="text-xl uppercase font-bold tracking-wider">
+            </motion.span>
+            <motion.span
+            initial={{opacity: 0, x: -10}}
+            animate={{opacity: 1, x:0}}
+            transition={{
+                animation: "easeInOut",
+                duration: 0.5,
+                delay: 0.15
+            }}
+            className="text-xl uppercase font-bold tracking-wider">
                 Coming Soon
-            </div>
+            </motion.span>
         </div>
     )
 }
