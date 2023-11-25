aaf3f2eefd70497811469ccc2a3c2f8ea8f43358
Vedant Bhoyar <vedantby@gmail.com>
2023-11-25 11:57:20 +0530

    feat: gtag

--- src/components/Analytics.tsx
+++ src/components/Analytics.tsx
@@ -1,7 +1,24 @@
 "use client"
 
+import Script from "next/script"
 import { Analytics as VercelAnalytics } from "@vercel/analytics/react"
 
-const Analytics = () => <VercelAnalytics />
+const gtag = "G-C93H001C82"
+
+const Analytics = () => (
+    <>
+        <VercelAnalytics />
+        <Script src="https://www.googletagmanager.com/gtag/js?id=${gtag}" />
+        <Script id="google-analytics">
+            {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+ 
+          gtag('config', '${gtag}');
+        `}
+        </Script>
+    </>
+)
 
 export default Analytics
