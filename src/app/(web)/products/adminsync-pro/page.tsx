8ce7c0d03e3beb3a341f2eb4efc66aa2f1b3eee1
Vedant Bhoyar <vedantby@gmail.com>
2023-12-14 16:55:37 +0530

    fix: adminsync page title

--- src/app/(web)/products/adminsync-pro/page.tsx
+++ src/app/(web)/products/adminsync-pro/page.tsx
@@ -45,7 +45,7 @@ const page = (props: Props) => {
                                 Adminstrative Integrated Management System
                             </h2>
                             <h1 className="mb-1 text-3xl font-medium text-primary title-font">
-                                AdminSync Pro
+                                AdminSync Pro - Integrated Administrative System
                             </h1>
                             <div className="flex mb-4">
                                 <span className="flex items-center">
@@ -112,7 +112,7 @@ const page = (props: Props) => {
                                     </span>
                                 </span>
                                 <span className="flex py-2 pl-3 ml-3 border-l-2 border-gray-200 space-x-2s">
-                                    <a className="text-muted-foreground">
+                                    <span className="text-muted-foreground">
                                         <svg
                                             fill="currentColor"
                                             strokeLinecap="round"
@@ -123,8 +123,8 @@ const page = (props: Props) => {
                                         >
                                             <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
                                         </svg>
-                                    </a>
-                                    <a className="text-muted-foreground">
+                                    </span>
+                                    <span className="text-muted-foreground">
                                         <svg
                                             fill="currentColor"
                                             strokeLinecap="round"
@@ -135,8 +135,8 @@ const page = (props: Props) => {
                                         >
                                             <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
                                         </svg>
-                                    </a>
-                                    <a className="text-muted-foreground">
+                                    </span>
+                                    <span className="text-muted-foreground">
                                         <svg
                                             fill="currentColor"
                                             strokeLinecap="round"
@@ -147,7 +147,7 @@ const page = (props: Props) => {
                                         >
                                             <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
                                         </svg>
-                                    </a>
+                                    </span>
                                 </span>
                             </div>
                             <p className="leading-relaxed">
