81cc6591783e7804d77c74e8cabf235b74dc5053
Vedant Bhoyar <vedantby@gmail.com>
2023-12-17 21:40:49 +0530

    fix: main page description short

--- src/config/site.ts
+++ src/config/site.ts
@@ -4,7 +4,7 @@ import { SiteConfig } from "@/types"
 export const siteConfig: SiteConfig = {
     name: "Kodecs Technosoft",
     description:
-        "Welcome to Kodecs Technosoft, where innovation meets excellence. Explore our cutting-edge software solutions. Empower your business with user-friendly software tools and subscribe to our industry-leading products. Transform your digital landscape with Kodecs Technosoft.",
+        "Welcome to Kodecs Technosoft, where innovation meets excellence. Explore our cutting-edge software solutions, empower your business with user-friendly software tools. Transform your digital landscape with Kodecs Technosoft.",
     tagline: "Empowering innovation into reality",
     url: env.NEXT_PUBLIC_APP_BASE_URL || "https://kodecs.in",
     ogImage: `${env.NEXT_PUBLIC_APP_BASE_URL}/og-image.png`,
