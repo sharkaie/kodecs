90a0f596c9a6b37403dba30a81284e5bb29b6ee8
Vedant Bhoyar <vedantby@gmail.com>
2023-12-19 00:23:13 +0530

    fix: mobile nav border and backdrop blue

--- src/components/web/mobile-nav.tsx
+++ src/components/web/mobile-nav.tsx
@@ -25,10 +25,10 @@ export function MobileNav({
         <>
             <div
                 className={cn(
-                    "fixed inset-0 top-16 z-50 grid h-[calc(100vh-4rem)] grid-flow-row auto-rows-max overflow-auto p-6 pb-32 shadow-md border border-border animate-in slide-in-from-bottom-80 md:hidden"
+                    "fixed inset-0 top-16 z-50 grid h-[calc(100vh-4rem)] grid-flow-row auto-rows-max overflow-auto p-6 pb-32 shadow-md animate-in slide-in-from-bottom-80 md:hidden backdrop-blur"
                 )}
             >
-                <div className="relative z-20 grid gap-6 p-4 rounded-md shadow-md bg-popover text-popover-foreground">
+                <div className="relative z-20 grid gap-6 p-4 rounded-md shadow-md bg-popover text-popover-foreground border border-border">
                     <nav className="grid grid-flow-row text-sm auto-rows-max">
                         {items.map((item, index) => (
                             <Link
